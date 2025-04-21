8094d0c251d98484a970ffa7eb2716a4012a74ce
bsruthibv <bsruthiuk@gmail.com>
2025-04-21 17:17:33 +0100

    modified main.tf with instance creation

--- day-4-statefile-remote/main.tf
+++ day-4-statefile-remote/main.tf
@@ -11,3 +11,13 @@ resource "aws_subnet" "subnet-2" {
     cidr_block = "10.0.1.0/24"
     vpc_id = aws_vpc.VPC.id
 }
+
+resource "aws_instance" "Public_server" {
+    ami = "ami-002f6e91abff6eb96"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "public-server"
+    }
+  
+}
