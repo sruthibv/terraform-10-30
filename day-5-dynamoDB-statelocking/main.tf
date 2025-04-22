572eb7aa67696671b8de169a076911de343764f4
bsruthibv <bsruthiuk@gmail.com>
2025-04-22 22:42:51 +0100

    updated

--- day-5-dynamoDB-statelocking/main.tf
+++ day-5-dynamoDB-statelocking/main.tf
@@ -1,8 +1,23 @@
 resource "aws_instance" "name" {
   ami = "ami-002f6e91abff6eb96"
   instance_type = "t2.micro"
+  availability_zone = "ap-south-1b"
 
   tags = {
-    Name = "test"
+    Name = "pub"
   }
+
+#   lifecycle {
+# 	prevent_destroy = true
+# }
+
+#   lifecycle {
+#     ignore_changes = [tags,] #This means that Terraform will never update the object but will be able to create or destroy it.
+#   }
+
+# lifecycle {
+#      create_before_destroy = true    #this attribute will create the new object first and then destroy the old one
+#    }
+
+
 }
